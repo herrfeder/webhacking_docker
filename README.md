d07be65b3e0bbb61fa9f485cd41bcbcb21c4712b
herrfeder <d.lassig@t-online.de>
2023-04-24 21:39:19 +0200

    rebuild of repository

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # webhacking_docker
-Container for Webhacking with vnc and ssh
+
+Kali Linux based Container Image for Webhacking and Source code Review. (created for OSWE course)
+
+  * includes everything to work on the OSWE Labs
+    * (the only thing is Python Remote Debugging which doesn't work properly with included Codeserver)
+  * spawns multiple Applications via supervisord
+  * can be used via
+    * VNC
+    * SSH
+    * pre-installed Codeserver (web-based VSCode)
+
+## Screenshots
+
+| Codeserver | VNC Access |
+|--------------------------------------|--------------------------------------|
+| ![](screenshots/codeserver.png) | ![](screenshots/vnc.png) |
+
+## How to use
+
+  * Rename `webhacking.template` to `webhacking.env` and fill values
+  * Start Container with:
+    * `docker-compose up -d`
+  * Connect SSH via:
+    * `ssh -p2222 user@127.0.0.1`
+  * Connect VNC via:
+    * `vnc://127.0.0.1:5900`
+
+## Mentions
+
+  * Container-Image is based on https://github.com/fcwu/docker-ubuntu-vnc-desktop modified to use with Kali
+  * Web-based Editor is Codeserver: https://github.com/coder/code-server
+  * Awesome Desktop Wallpaper from: https://github.com/owerdogan/wallpapers-for-kali
--- webhacking-build/rootfs/startup.sh
+++ webhacking-build/rootfs/startup.sh
@@ -12,10 +12,6 @@ if [ -n "$X11VNC_ARGS" ]; then
     sed -i "s/^command=x11vnc.*/& ${X11VNC_ARGS}/" /etc/supervisor/conf.d/supervisord.conf
 fi
 
-if [ -n "$OPENBOX_ARGS" ]; then
-    sed -i "s#^command=/usr/bin/openbox.*#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
-fi
-
 if [ -n "$RESOLUTION" ]; then
     sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
 fi
@@ -31,7 +27,6 @@ if [ "$USER" != "root" ]; then
     fi
     HOME=/home/$USER
     echo "$USER:$PASSWORD" | chpasswd
-    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} ${HOME}
     chown -R $USER:$USER ${HOME}
     [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
 fi
@@ -40,42 +35,15 @@ sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervis
 # home folder
 if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
     mkdir -p $HOME/.config/pcmanfm/LXDE/
-    ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/
+    ln -sf /usr/local/share/wallpapers/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/
     chown -R $USER:$USER $HOME
 fi
 
-# nginx workers
-sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
-
-# nginx ssl
-if [ -n "$SSL_PORT" ] && [ -e "/etc/nginx/ssl/nginx.key" ]; then
-    echo "* enable SSL"
-	sed -i 's|#_SSL_PORT_#\(.*\)443\(.*\)|\1'$SSL_PORT'\2|' /etc/nginx/sites-enabled/default
-	sed -i 's|#_SSL_PORT_#||' /etc/nginx/sites-enabled/default
-fi
-
-# nginx http base authentication
-if [ -n "$HTTP_PASSWORD" ]; then
-    echo "* enable HTTP base authentication"
-    htpasswd -bc /etc/nginx/.htpasswd $USER $HTTP_PASSWORD
-	sed -i 's|#_HTTP_PASSWORD_#||' /etc/nginx/sites-enabled/default
-fi
-
-# dynamic prefix path renaming
-if [ -n "$RELATIVE_URL_ROOT" ]; then
-    echo "* enable RELATIVE_URL_ROOT: $RELATIVE_URL_ROOT"
-	sed -i 's|#_RELATIVE_URL_ROOT_||' /etc/nginx/sites-enabled/default
-	sed -i 's|_RELATIVE_URL_ROOT_|'$RELATIVE_URL_ROOT'|' /etc/nginx/sites-enabled/default
-fi
+mkdir -p /data/vscode-data
 
 # clearup
 PASSWORD=
+VNC_PASSWORD=
 HTTP_PASSWORD=
-
-
-#git clone https://github.com/herrfeder/vimconfig.git
-#mv vimconfig/tmux.conf ~/.tmux.conf
-#rm -rf vimconfig
-#chown user:user ~/.tmux.conf
 
 exec /bin/tini -- supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
